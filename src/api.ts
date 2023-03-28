38a4a3bd80da15a4e70a96c6a5e768b6b0adda46
Sergey-Saprankov <sergey-saprankov@mail.ru>
2023-03-28 21:50:43 +0300

    start proj

---
+++ src/api.ts
@@ -0,0 +1,17 @@
+import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
+import {store} from "./store";
+
+// Define a service using a base URL and expected endpoints
+export const pokemonApi = createApi({
+    reducerPath: 'pokemonApi',
+    baseQuery: fetchBaseQuery({ baseUrl: 'https://www.googleapis.com/books/v1/volumes/'}),
+    endpoints: (builder) => ({
+        getPokemonByName: builder.query({
+            query: (name) => `7zZQEAAAQBAJ?key=AIzaSyAd0gSPQumRnk_RECGKGr9AdAfo_JrNOPk`,
+        }),
+    }),
+})
+
+// Export hooks for usage in functional components, which are
+// auto-generated based on the defined endpoints
+export const { useGetPokemonByNameQuery } = pokemonApi
