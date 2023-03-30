491db9d64f5335f391d14d5330432d4016cf4107
Sergey-Saprankov <sergey-saprankov@mail.ru>
2023-03-30 14:07:34 +0300

    first

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,11 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
-import './index.css'
-import {Provider} from "react-redux";
-import {store} from "./store";
+import { createRoot } from 'react-dom/client'
+import { Provider } from 'react-redux'
 
-ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
-    <Provider store={store}><App/></Provider>
+import { App } from './App'
+import { store } from './store'
 
+createRoot(document.getElementById('root') as HTMLElement).render(
+  <Provider store={store}>
+    <App />
+  </Provider>
 )
